2a22a1745cc9fab82f5a3df84385013f054bd3cd
Anthony Lovern <voltorodarkwatch@gmail.com>
2023-12-05 13:12:28 -0500

    Through error handling middleware lecture

--- demo_code/app.js
+++ demo_code/app.js
@@ -18,17 +18,18 @@ const checkUserInput = (req, res, next) => {
     };
 }
 
-app.use((req, res, next) => {
-    console.log('error test');
-    const error = 'There was an error :(';
-    next(error);
-})
+// app.use((req, res, next) => {
+//     console.log('error test');
+//     const error = new Error('There was an error :(');
+//     error.statusCode = 401;
+//     next(error);
+// })
 
 app.get(['/test', '/status'], printPath, (err, req, res, next) => {
     res.send('Hello from your first express API!')
 });
 
-app.post('/create', printPath, checkUserInput, (req, res) => {
+app.post('/create', [printPath, checkUserInput], (req, res) => {
     console.log(req.body)
     res.json(req.body)
 });
@@ -45,10 +46,21 @@ app.get('/actors', (req, res) => {
     console.log(req.query)
 })
 
+app.use((req, res, next) => {
+    const notFoundError = new Error(`${req.method} ${req.path} not found`);
+    notFoundError.statusCode = 404;
+    next(notFoundError)
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.send(err)
-})
+    console.log(err.message);
+    const status = err.statusCode || 500;
+    res.status(status);
+    res.json({
+        message: err.message || 'Something went wrong...',
+        status
+    })
+});
 
 app.listen(8000, () => console.log('Listening on port 8000...'));
